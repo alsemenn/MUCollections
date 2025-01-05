1182ef239aa5b970540be4517eb136d64cdebf58
Sasha Semennikov <sasha.semennikov@outlook.com>
2025-01-04 21:37:07 -0500

    Fix .gitignore

---
+++ MUCollections.Common.Test/Trees/AVLTreeTest.cs
@@ -0,0 +1,14 @@
+﻿using MUCollections.Common.Trees;
+
+namespace MUCollections.Common.Test.Trees
+{
+	public class AVLTreeTest
+	{
+		[Fact]
+		public void Test_Empty()
+		{
+			var obj = new AVLTree<int, int>();
+			Assert.Empty(obj);
+		}
+	}
+}
