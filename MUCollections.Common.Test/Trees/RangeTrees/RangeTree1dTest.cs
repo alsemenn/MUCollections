258f3cb4969ed248896769b726c876394aa565da
Sasha Semennikov <sasha.semennikov@outlook.com>
2025-01-08 15:38:19 -0500

    Add 1D Range Tree

---
+++ MUCollections.Common.Test/Trees/RangeTrees/RangeTree1dTest.cs
@@ -0,0 +1,26 @@
+﻿using MUCollections.Common.Trees.RangeTrees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MUCollections.Common.Test.Trees.RangeTrees
+{
+	public class RangeTree1dTest
+	{
+		[Fact]
+		public void Test_EmptyTree()
+		{
+			var tree = new RangeTree1D<int, int>(Array.Empty<(int, int)>());
+			Assert.Empty(tree.GetValues(0, 1000));
+		}
+
+		[Fact]
+		public void Test_Single()
+		{
+			var testObj = new RangeTree1D<int, int>(new[] { (1, 1) });
+			Assert.Equal(new[] { (1, 1) }, testObj.GetValues(0, 100));
+		}
+	}
+}
---
+++ MUCollections.Common/Trees/RangeTrees/RangeTree1D.cs
@@ -0,0 +1,89 @@
+﻿namespace MUCollections.Common.Trees.RangeTrees
+{
+	/// <summary>
+	/// A read-only structure that can return all values in the given range. </br>
+	/// Leaf nodes will contain (TKey, TValue). All leaf nodes will be connected into a single-linked-list.
+	/// Structure of a mid node is:
+	///     A
+	///   / | \
+	///  L  M  R
+	///  
+	/// Where L &lt A &lt R. L will contain all keys less then A. R will contain all keys greater than A. And M is a list containing all keys equal to A.
+	/// </summary>
+	public class RangeTree1D<TKey, TValue>
+	{
+		private readonly IComparer<TKey> _comparer = Comparer<TKey>.Default;
+
+		private readonly Node? _root;
+
+		public RangeTree1D(IEnumerable<(TKey, TValue)> values)
+		{
+			var list = new LinkedList<(TKey key, TValue value)>(values);
+			
+			if (list.Count == 0)
+			{
+				return;
+			}
+			var queue = new Queue<Node>();
+			Node? prev = null;
+			for (var cur = list.First; cur != null; cur = cur.Next)
+			{
+				if (prev == null)
+				{
+					prev = new Node
+					{
+						Key = cur.Value.key,
+						Leaf = cur
+					};
+					queue.Enqueue(prev);
+					continue;
+				}
+				if (_comparer.Compare(prev.Key, cur.Value.key) == 0)
+				{
+					continue;
+				}
+				prev = new Node
+				{
+					Key = cur.Value.key,
+					Leaf = cur
+				};
+				queue.Enqueue(prev);
+			}
+
+			while (queue.Count == 1)
+			{
+				var left = queue.Dequeue();
+				var t = queue.Dequeue();
+				var right = (queue.Count > 0) ? queue.Dequeue() : null;
+				var node = new Node
+				{
+					Key = t.Key,
+					Leaf = t.Leaf,
+					Left = left,
+					Right = right
+				};
+			}
+		}
+
+
+		/// <summary>
+		/// Returns all values in the given closed range. </br>
+		/// </summary>
+		/// <param name="from"> Range start included. </param>
+		/// <param name="to"> Range end included. </param>
+		/// <returns></returns>
+		public IEnumerable<(TKey key, TValue value)> GetValues(TKey from, TKey to)
+		{
+			yield break;
+		}
+
+		internal class Node
+		{
+			public required TKey Key { get; init; }
+			public required LinkedListNode<(TKey key, TValue value)> Leaf { get; init; }
+
+			public Node? Left { get; init; }
+			public Node? Right { get; init; }
+		}
+	}
+}
